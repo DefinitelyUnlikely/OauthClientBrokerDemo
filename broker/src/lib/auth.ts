fd75a21e7462d7bc12f08a9ab21e683307ad5aef
martin kåår <martin.kaar@emcap.se>
2025-11-26 11:06:58 +0100

    add github as social login provider

--- broker/src/lib/auth.ts
+++ broker/src/lib/auth.ts
@@ -2,6 +2,10 @@ import { betterAuth } from 'better-auth';
 import { MssqlDialect } from 'kysely';
 import * as Tedious from 'tedious';
 import * as Tarn from 'tarn';
+import { sveltekitCookies } from 'better-auth/svelte-kit';
+import { getRequestEvent } from '$app/server';
+
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 
 const dialect = new MssqlDialect({
 	tarn: {
@@ -36,5 +40,12 @@ export const auth = betterAuth({
 	database: {
 		dialect,
 		type: 'mssql'
-	}
+	},
+	socialProviders: {
+		github: {
+			clientId: GITHUB_CLIENT_ID,
+			clientSecret: GITHUB_CLIENT_SECRET
+		}
+	},
+	plugins: [sveltekitCookies(getRequestEvent)] // make sure sveltekitCookies is last in the array
 });
