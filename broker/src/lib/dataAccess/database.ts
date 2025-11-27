4bfea6cbb036f73587774b1f5441bb8cf2b6dc75
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-11-27 13:58:04 +0100

    add a database.ts file with creation of mssql dialect + custom tables for oauth

---
+++ broker/src/lib/dataAccess/database.ts
@@ -0,0 +1,64 @@
+import { MssqlDialect, Kysely } from 'kysely';
+import * as Tedious from 'tedious';
+import * as Tarn from 'tarn';
+import {
+	MSSQL_SERVER,
+	MSSQL_DATABASE,
+	MSSQL_PORT,
+	MSSQL_USER,
+	MSSQL_PASSWORD
+} from '$env/static/private';
+
+export interface OAuthClient {
+	id: string;
+	secret: string;
+	redirect_uri: string;
+	name: string;
+}
+
+export interface OAuthCode {
+	code: string;
+	client_id: string;
+	redirect_uri: string;
+	expires_at: number;
+	code_challenge: string;
+	code_challenge_method: string;
+}
+
+interface OAuthTables {
+	oauth_clients: OAuthClient;
+	oauth_codes: OAuthCode;
+}
+
+export const dialect = new MssqlDialect({
+	tarn: {
+		...Tarn,
+		options: {
+			min: 0,
+			max: 10
+		}
+	},
+	tedious: {
+		...Tedious,
+		connectionFactory: () =>
+			new Tedious.Connection({
+				authentication: {
+					options: {
+						password: MSSQL_PASSWORD,
+						userName: MSSQL_USER
+					},
+					type: 'default'
+				},
+				options: {
+					database: MSSQL_DATABASE,
+					port: Number(MSSQL_PORT),
+					trustServerCertificate: true
+				},
+				server: MSSQL_SERVER
+			})
+	}
+});
+
+export const db = new Kysely<OAuthTables>({
+	dialect
+});
