6da627404dad5b8a66643b7d78ae19ac93c23396
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-11-28 14:13:50 +0100

    add a POST endpoint to the clients logout server, to test a way to logout all apps at once

--- broker/src/routes/logout/+server.ts
+++ broker/src/routes/logout/+server.ts
@@ -4,9 +4,13 @@ import { redirect, type RequestHandler } from '@sveltejs/kit';
 export const GET: RequestHandler = async ({ url, request }) => {
 	const redirectTo = url.searchParams.get('redirect_uri');
 
+	// let better-auth handle logout for the broker
 	await auth.api.signOut({
 		headers: request.headers
 	});
+
+	// Now, we need to contact all the service providers
+	// to let them know that the user has logged out and they need to invalidate the session
 
 	if (redirectTo) {
 		redirect(302, redirectTo);
--- client/src/routes/logout/+server.ts
+++ client/src/routes/logout/+server.ts
@@ -5,3 +5,9 @@ export const GET: RequestHandler = async ({ cookies }) => {
 	cookies.delete('oauth_id_token', { path: '/' });
 	throw redirect(302, `${BROKER_URL}/logout?redirect_uri=${BASE_URL}`);
 };
+
+// Use the post method to logout from the broker
+export const POST: RequestHandler = async ({ cookies }) => {
+	cookies.delete('oauth_id_token', { path: '/' });
+	return new Response(null, { status: 200 });
+};
